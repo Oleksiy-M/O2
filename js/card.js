3d38b6c93a3d79b207de57e0284e3a786409ee91
Oleksiy-M <105721227+Oleksiy-M@users.noreply.github.com>
2024-02-01 04:54:57 +0200

    history

---
+++ js/card.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+   const cardsList = document.querySelector('.cards');
+   const cards = document.querySelectorAll('.cards__item');
+
+   cards.forEach(card => {
+      const clone = card.cloneNode(true);
+      cardsList.appendChild(clone);
+   });
+}
+
+
+// document.querySelector('.section-card').addEventListener('mouseenter', function () {
+//    const cards = document.querySelectorAll('.cards');
+//    cards.forEach(card => card.classList.add('paused'));
+// });
+
+// document.querySelector('.section-card').addEventListener('mouseleave', function () {
+//    const cards = document.querySelectorAll('.cards');
+//    cards.forEach(card => card.classList.remove('paused'));
+// });
