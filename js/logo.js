efaf54916d1baf59ace679a88a68cac99148bf78
Oleksiy-M <105721227+Oleksiy-M@users.noreply.github.com>
2024-12-02 23:59:48 +0200

    fix

--- js/logo.js
+++ js/logo.js
@@ -4,7 +4,7 @@ const heroLogo = document.querySelector('.hero-logo');
 // const waterLogo = document.querySelector('.water-logo__block');
 
 // Налаштовуємо Intersection Observer
-const observer = new IntersectionObserver(entries => {
+const observerLogo = new IntersectionObserver(entries => {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
 			// Додаємо клас active при появі на екрані
@@ -17,33 +17,49 @@ const observer = new IntersectionObserver(entries => {
 	});
 });
 
-// Запускаємо спостереження за елементом
-observer.observe(heroLogo);
-// observer.observe(waterLogo);
+observerLogo.observe(heroLogo);
 
 
-document.addEventListener('DOMContentLoaded', () => {
-	const titleBox = document.querySelector('.hero-title__box'); // Елемент, за яким спостерігаємо
 
-	// Налаштовуємо IntersectionObserver
-	const observer = new IntersectionObserver(
-		entries => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					titleBox.classList.add('active');
-					// Додаємо анімацію через JavaScript, коли елемент з'являється на екрані
-					// titleBox.style.transition = '$anime-text'; // Стиль анімації
-					titleBox.style.transform = 'translateX(0vw)'; // Переміщаємо елемент на екран
-					titleBox.style.opacity = '1'; // Робимо елемент видимим
-				} else {
-					// Приховуємо елемент, коли він йде з екрану
-					titleBox.style.transform = 'translateX(-50vw)';
-					titleBox.style.opacity = '0';
-				}
-			});
-		},
-		{ threshold: 0.1 } // 50% елемента має бути видно
-	);
 
-	observer.observe(titleBox); // Спостерігаємо за елементом
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const titleBox = document.querySelector('.hero-title__box'); // Елемент, за яким спостерігаємо
+
+// 	// Налаштовуємо IntersectionObserver
+// 	const observer = new IntersectionObserver(
+// 		entries => {
+// 			entries.forEach(entry => {
+// 				if (entry.isIntersecting) {
+// 					titleBox.classList.add('active');
+// 					// Додаємо анімацію через JavaScript, коли елемент з'являється на екрані
+// 					// titleBox.style.transition = '$anime-text'; // Стиль анімації
+// 					titleBox.style.transform = 'translateY(-50px)'; // Переміщаємо елемент на екран
+// 					titleBox.style.opacity = '0'; // Робимо елемент видимим
+// 				} else {
+// 					// Приховуємо елемент, коли він йде з екрану
+// 					titleBox.style.transform = 'translateY(0vw)';
+// 					titleBox.style.opacity = '1';
+// 				}
+// 			});
+// 		},
+// 		{ threshold: 0.1 } // 50% елемента має бути видно
+// 	);
+
+// 	observer.observe(titleBox); // Спостерігаємо за елементом
+// });
+
+
+const titleLogo = document.querySelector('.title__logo');
+
+// Налаштовуємо Intersection Observer
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			// Додаємо клас active при появі на екрані
+			titleLogo.classList.add('active2');
+		} 
+	});
 });
+
+// Запускаємо спостереження за елементом
+observer.observe(titleLogo);
